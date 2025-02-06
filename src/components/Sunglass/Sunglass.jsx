ad609376b186d594b0e891cc561473156011612c
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-02-07 01:12:57 +0600

    updated code

---
+++ src/components/Sunglass/Sunglass.jsx
@@ -0,0 +1,11 @@
+import Watch from '../Watch/Watch';
+import './Sunglass.css';
+const Sunglass = () => {
+    return (
+        <div>
+            <Watch></Watch>
+        </div>
+    );
+};
+
+export default Sunglass;
