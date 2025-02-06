cc1c6d485e4fd0704f9ee21af37f2f7fea9fdefd
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-02-07 01:26:02 +0600

    added new code file

--- src/components/Sunglass/Sunglass.jsx
+++ src/components/Sunglass/Sunglass.jsx
@@ -1,6 +1,12 @@
+import add from '../../Utils/calculate';
 import Watch from '../Watch/Watch';
 import './Sunglass.css';
 const Sunglass = () => {
+
+    const first = 55;
+    const second = 165;
+    const sum = add(first, second);
+
     return (
         <div>
             <Watch></Watch>
